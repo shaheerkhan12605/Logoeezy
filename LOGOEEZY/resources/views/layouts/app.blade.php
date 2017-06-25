6c43d0527f0817b2b04abf2f98547ebed24d1341
Shaheer Khan <shaheer.khan@emumba.com>
2017-06-25 17:50:01 +0500

    working on home page

--- LOGOEEZY/resources/views/layouts/app.blade.php
+++ LOGOEEZY/resources/views/layouts/app.blade.php
@@ -21,67 +21,266 @@
     </script>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+  <div id="app">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ url('/login') }}">Login</a></li>
-                            <li><a href="{{ url('/register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ url('/logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Logoeezy') }}
+                </a>
             </div>
-        </nav>
 
-        @yield('content')
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    &nbsp;
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href="{{ url('/logout') }}"
+                                        onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+
+                                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+        <div class="container ">
+            <div class="navbar-header">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse1">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand logo-category">
+                    {{ config('app.name', 'Logoeezy') }}
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse1">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    &nbsp;
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav ">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            Dropdown 1<span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href=""> sample1</a>
+                            </li>
+                            <li>
+                                <a href=""> sample2</a>
+                            </li>
+                            <li>
+                                <a href=""> sample3</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            Dropdown 2<span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href=""> sample1</a>
+                            </li>
+                            <li>
+                                <a href=""> sample2</a>
+                            </li>
+                            <li>
+                                <a href=""> sample3</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            Dropdown 3<span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href=""> sample1</a>
+                            </li>
+                            <li>
+                                <a href=""> sample2</a>
+                            </li>
+                            <li>
+                                <a href=""> sample3</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="content">
+      @yield('content')
     </div>
-
-    <!-- Scripts -->
-    <script src="/js/app.js"></script>
+    <section class="footer-section  blackbg-light footer">
+      <div class="container-fluid footer-container">
+        <div class="row">
+          <div class=" col-md-4  col-sm-4 text-center">
+            <img src="favicon.ico" style="height: 50px;width: 50px;background-color: white;" />
+            <p class="font-size-15">LOGOEEZY is a site where you can find any logo or t-shirt design on convert image to vectot if you want </p>
+            <br/>
+            <ul class="footer-links">
+              <li>
+                <a target="_blank" href="#">
+                  <i class="fa fa-facebook fb"></i>
+                </a>
+              </li>
+              <li>
+                <a target="_blank" href="#">
+                  <i class="fa fa-twitter twt"></i>
+                </a>
+              </li>
+              <li>
+                <a target="_blank" href="#">
+                  <i class="fa fa-linkedin ln"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div >
+            <div class="col-md-8 col-sm-8">
+              <div class="col-xs-12 col-sm-4 col-md-3">
+                <div class="clearfix">
+                  <h4> TShirts</h4>
+                </div>
+                <div class="clearfix padding-left-0">
+                  <div class="col-md-12 padding-left-0">
+                    sample1
+                  </div>
+                  <div class="col-md-12 padding-left-0">
+                    sample2
+                  </div>
+                  <div class="col-md-12 padding-left-0">
+                    sample3
+                  </div>
+                  <div class="col-md-12 padding-left-0">
+                    sample4
+                  </div>
+                </div>
+              </div>
+              <div class="col-xs-12 col-sm-4 col-md-3">
+                <div class="clearfix">
+                  <h4> TShirts</h4>
+                </div>
+                <div class="clearfix padding-left-0">
+                  <div class="col-md-12 padding-left-0">
+                    sample1
+                  </div>
+                  <div class="col-md-12 padding-left-0">
+                    sample2
+                  </div>
+                  <div class="col-md-12 padding-left-0">
+                    sample3
+                  </div>
+                  <div class="col-md-12 padding-left-0">
+                    sample4
+                  </div>
+                </div>
+              </div><div class="col-xs-12 col-sm-4 col-md-3">
+                <div class="clearfix">
+                  <h4> TShirts</h4>
+                </div>
+                <div class="clearfix padding-left-0">
+                  <div class="col-md-12 padding-left-0">
+                    sample1
+                  </div>
+                  <div class="col-md-12 padding-left-0">
+                    sample2
+                  </div>
+                  <div class="col-md-12 padding-left-0">
+                    sample3
+                  </div>
+                  <div class="col-md-12 padding-left-0">
+                    sample4
+                  </div>
+                </div>
+              </div><div class="col-xs-12 col-sm-4 col-md-3">
+                <div class="clearfix">
+                  <h4> TShirts</h4>
+                </div>
+                <div class="clearfix padding-left-0">
+                  <div class="col-md-12 padding-left-0">
+                    sample1
+                  </div>
+                  <div class="col-md-12 padding-left-0 ">
+                    sample2
+                  </div>
+                  <div class="col-md-12 padding-left-0">
+                    sample3
+                  </div>
+                  <div class="col-md-12 padding-left-0">
+                    sample4
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="clearfix end-bg">
+        <div class="col-md-4 col-sm-4">
+          <p>Copyright Logoeezy 2016.All Rights Reserved.</p>
+        </div>
+        <div class="col-md-8 col-sm-8 text-right">
+          <ul class="end-footer">
+            <li><a href="">Home</a></li>
+            <li><a href="">About us</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Privacy Policy</a></li>
+            <li><a href="">Terms & Condition</a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  </div>
+  <!-- Scripts -->
+  <script src="/js/app.js"></script>
 </body>
 </html>
--- LOGOEEZY/resources/views/welcome.blade.php
+++ LOGOEEZY/resources/views/welcome.blade.php
@@ -1,91 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Logoeezy</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+@section('content')
+    <div class="content-search ">
+        <div class="clearfix">
+            <div class="col-xs-8 col-sm-7 col-md-7 col-md-offset-2 col-sm-offset-2 col-xs-offset-1 text-center search-bar padding-right-0"> 
+                    <input type="text" name="name" class="form-control">
+            </div>
+            <div class="col-xs-2 col-sm-1 col-md-1 search-bar padding-left-0">
+                <button class="btn btn-default btn-search">Search</button>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+    <div class="clearfix">
+        <div class="col-md-3 col-sm-4 col-xs-6">
+            <img src="favicon.ico" class=" thumbnail img-responsive">
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+            <img src="favicon.ico" class=" thumbnail img-responsive">
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+            <img src="favicon.ico" class="thumbnail img-responsive">
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+            <img src="favicon.ico" class="thumbnail img-responsive">
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+            <img src="favicon.ico" class="thumbnail img-responsive">
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+            <img src="favicon.ico" class="thumbnail img-responsive">
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+            <img src="favicon.ico" class="thumbnail img-responsive">
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+            <img src="favicon.ico" class="thumbnail img-responsive">
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+            <img src="favicon.ico" class="thumbnail img-responsive">
+        </div>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+            <img src="favicon.ico" class="thumbnail img-responsive">
+        </div>
+    </div>
+@endsection
